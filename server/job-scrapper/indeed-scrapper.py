f0b6f5fc3be99be8e311480a3ebc1f3f6151a78f
thedarianwong <thedarianwong@gmail.com>
2023-10-23 01:08:54 -0700

    Scraped some job titles but still slow

--- server/job-scrapper/indeed-scrapper.py
+++ server/job-scrapper/indeed-scrapper.py
@@ -1,14 +1,67 @@
-# import requests
-from bs4 import BeautifulSoup4
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+import sys
+import random
 
-def get_url(position,location):
+
+
+def get_url(position, location):
+    
     template = "https://www.indeed.ca/jobs?q={}&l={}"
-    url = template.format(position.replace(' ', '+'), location.replace(' ', '+'))
-    print (url)
+    url = template.format(position.replace(' ', '+'),
+                          location.replace(' ', '+'))
+    print(url)
     return url
+
+def get_job_data(driver, target_url):
+    driver.get(target_url)
+    time.sleep(random.uniform(8.5, 10.9))
+    try:
+        close = driver.find_element('xpath', '//*[@id="mosaic-desktopserpjapopup"]/div[1]/button')
+        close.click()
+    except:
+        pass
+
+
+    all_data = []
+
+    job_cards = driver.find_elements('xpath', '//*[@id="mosaic-provider-jobcards"]/ul' )
+
+    for card in job_cards:
+        card.location_once_scrolled_into_view
+        card.click()
+        time.sleep(random.uniform(4.6, 6.9))
+   
+        job_title = driver.find_element('xpath', '//*[@id="mosaic-provider-jobcards"]/ul/li[1]/div/div[1]/div/div[1]/div/table[1]/tbody/tr/td/div[1]/h2' ).text
+        title = job_title.split('\n')
+        data = {'Job Title': title[0]}
+        all_data.append(data)
+    print(all_data)
 
 
 def main():
+    # PROXY = ""
+    # chrom_options = webdriver.ChromeOptions()
+    # chrom_options.add_argument(f'--proxy-server={PROXY}')
     position = 'Software Engineer Intern'
     location = 'Vancouver, BC'
-    get_url(position, location)
+    jobs = []
+    url =get_url(position, location)
+    browser = webdriver.Chrome()
+    for page in range(0, 50, 10):
+        order_page = (url + '&start=' +str(page))
+        browser.get(order_page)
+        time.sleep(random.uniform(8.5, 10.9))
+        # We need to add close modal button just in case if theres notifications popped out
+        get_job_data(browser, order_page)
+
+    browser.close()
+
+
+
+main()
