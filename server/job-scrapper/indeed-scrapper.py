756207b1b1188992485ba12cea667fb4ce8ad99c
thedarianwong <thedarianwong@gmail.com>
2023-10-23 01:48:41 -0700

    Scraped job title works

--- server/job-scrapper/indeed-scrapper.py
+++ server/job-scrapper/indeed-scrapper.py
@@ -12,7 +12,7 @@ import random
 
 def get_url(position, location):
     
-    template = "https://www.indeed.ca/jobs?q={}&l={}"
+    template = "https://www.indeed.ca/jobs?q={}&l={}&sort=date"
     url = template.format(position.replace(' ', '+'),
                           location.replace(' ', '+'))
     print(url)
@@ -30,16 +30,16 @@ def get_job_data(driver, target_url):
 
     all_data = []
 
-    job_cards = driver.find_elements('xpath', '//*[@id="mosaic-provider-jobcards"]/ul' )
+    job_cards = driver.find_elements('xpath', '//div[@class = "css-1m4cuuf e37uo190"]' )
 
     for card in job_cards:
         card.location_once_scrolled_into_view
         card.click()
         time.sleep(random.uniform(4.6, 6.9))
    
-        job_title = driver.find_element('xpath', '//*[@id="mosaic-provider-jobcards"]/ul/li[1]/div/div[1]/div/div[1]/div/table[1]/tbody/tr/td/div[1]/h2' ).text
+        job_title = driver.find_element('xpath', '//*[@id="jobsearch-ViewjobPaneWrapper"]/div/div/div/div[1]/div/div[1]/div[1]/h2' ).text.strip()
         title = job_title.split('\n')
-        data = {'Job Title': title[0]}
+        data = {'Job Title': title}
         all_data.append(data)
     print(all_data)
 
@@ -56,7 +56,7 @@ def main():
     for page in range(0, 50, 10):
         order_page = (url + '&start=' +str(page))
         browser.get(order_page)
-        time.sleep(random.uniform(8.5, 10.9))
+        time.sleep(random.uniform(2, 4))
         # We need to add close modal button just in case if theres notifications popped out
         get_job_data(browser, order_page)
 
