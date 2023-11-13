086e87f592043dfaf4dce22e7fb27b5fd8b8acf9
thedarianwong <thedarianwong@gmail.com>
2023-11-13 03:01:59 -0800

    added logo and axios to req jobs data from server

--- client/job-aggregator-client/src/App.js
+++ client/job-aggregator-client/src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import logo from "./jobhub_logo.png";
 
 function App() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/jobs") // Adjust the URL as needed
+      .then((response) => {
+        setJobs(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the jobs!", error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="JobHub" />
       </header>
+      <main>
+        <div className="table-container">
+          <table>
+            <thead>
+              <tr>
+                <th>Job Position</th>
+                <th>Company</th>
+                <th>Apply URL</th>
+              </tr>
+            </thead>
+            <tbody>
+              {jobs.map((job, index) => (
+                <tr key={index}>
+                  <td>{job.title}</td>
+                  <td>{job.company}</td>
+                  <td>
+                    <a
+                      href={job.job_url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Apply
+                    </a>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </main>
     </div>
   );
 }
--- server/job-server/app.py
+++ server/job-server/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
 from extensions import db
 from models import Job
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../../database/JobBoard.db'
 db.init_app(app)
@@ -10,7 +12,10 @@ db.init_app(app)
 @app.route('/jobs', methods=['GET'])
 def get_jobs():
     jobs = Job.query.all()
-    return jsonify([job.to_dict() for job in jobs])
+    jobs_data = [job.to_dict() for job in jobs]
+    if jobs_data:
+        jobs_data = jobs_data[1:]  # Skip the first row
+    return jsonify(jobs_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
